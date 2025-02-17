e6e6559fc769e4acc320cec449ae995a2f3d3dec
Kigula Jesse <jkigula@icloud.com>
2025-02-17 15:30:59 +0300

    pratising refactoring

---
+++ dist/tests.js
@@ -0,0 +1,8 @@
+"use strict";
+//-------------Arrow Functions
+//function add(a:number, b:number): number{
+//	return a + b;
+//}
+const add = (a, b) => (a + b);
+//console.log(add(1,2));
+const subtract = (x, b) => (x - b);
---
+++ src/tests.ts
@@ -0,0 +1,18 @@
+//-------------Arrow Functions
+//function add(a:number, b:number): number{
+//	return a + b;
+//}
+const add = (a:number,b:number):number => (a + b);
+//console.log(add(1,2));
+const subtract = (x:number, b:number)=> (x-b);
+//console.log(add(subtract(10,5),5));
+
+
+//-------------Generic Returns
+function identifyNumber()
+
+
+
+
+
+
